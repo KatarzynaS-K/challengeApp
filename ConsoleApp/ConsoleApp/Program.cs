430cbc501431f2555f0efa10a7baf40ed702c0cc
KatarzynaS-K <siekanowska@gmail.com>
2023-11-20 09:07:41 +0100

    praca domowa dzień 3

--- ConsoleApp/ConsoleApp/Program.cs
+++ ConsoleApp/ConsoleApp/Program.cs
@@ -1,2 +1,2 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Cześć! nazywam się Kasia. Zrobiłam pracę domową z trzeciego dnia wyzwania.");
