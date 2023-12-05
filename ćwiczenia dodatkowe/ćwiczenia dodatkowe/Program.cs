d4fa1fea6f78db2a2f9c3a6fd5d1b6c9b0c88179
KatarzynaS-K <siekanowska@gmail.com>
2023-12-05 12:09:42 +0100

    ćwiczenie dodatkowe

---
+++ ćwiczenia dodatkowe/ćwiczenia dodatkowe/Program.cs
@@ -0,0 +1,25 @@
+﻿
+using System;
+
+var age = 19;
+var gender = "men";
+
+if (gender == "women" && age > 18)
+{
+    Console.WriteLine("Pełnoletnia kobieta");
+}
+else if (gender == "women" && age < 18)
+{
+    Console.WriteLine("Niepełnoletnia dziewczyna");
+}
+else
+{
+    if (age > 18)
+    {
+        Console.WriteLine("Pełnoletni męższczyzna");
+    }
+    else
+    {
+        Console.WriteLine("Niepełnoletni chłopak");
+    }
+}
