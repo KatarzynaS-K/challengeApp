a263d2493416322e43222113dad724e8a49afae8
KatarzynaS-K <siekanowska@gmail.com>
2023-12-05 13:13:02 +0100

    tablica

--- ConsoleApp/ConsoleApp/Program.cs
+++ ConsoleApp/ConsoleApp/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Cześć! nazywam się Kasia. Zrobiłam pracę domową z trzeciego dnia wyzwania.");
+﻿int[] grades = new int[365];
+string[] dayOfWeeks = new string[7];
+
+dayOfWeeks[0] = "poniedziałek";
+dayOfWeeks[1] = "wtorek";
+dayOfWeeks[2] = "środa";
+dayOfWeeks[3] = "czwartekk";
+dayOfWeeks[4] = "piątek";
+dayOfWeeks[5] = "sobota";
+dayOfWeeks[6] = "niedziela";
+
+Console.WriteLine(dayOfWeeks[2]);
+
+string[] dayOfWeeks2 = { "poniedziałek", "wtorek", "środa", "czwartekk", "piątek", "sobota", "niedziela" }; 
+Console.WriteLine(dayOfWeeks2[4]);
