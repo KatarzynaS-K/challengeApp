84133fe1a2e70c14b91da1692ec449055f8c6ee0
KatarzynaS-K <siekanowska@gmail.com>
2023-12-05 11:13:44 +0100

    dzień 4

---
+++ Dzień 4/Dzień 4/Program.cs
@@ -0,0 +1,52 @@
+﻿var age = 26;
+var name = "Kasia";
+var gender = "women";
+if (gender == "women")
+{
+    if (name == "Kasia")
+    {
+        if (age == 33)
+        {
+            Console.WriteLine("Kobieta o imieniu Kasia,lat 33");
+        }
+        else if (age > 33)
+        {
+            Console.WriteLine("Kobieta o imieniu Kasia powyżej trzydziestu trzech lat");
+        }
+        else if (age < 33)
+        {
+            Console.WriteLine("Kobieta o imieniu Kasia poniżej trzydziestu trzech lat");
+        }
+    }
+    else
+    {
+        if (age == 33)
+        {
+            Console.WriteLine("Kobieta lat 33");
+        }
+        else if (age > 33)
+        {
+            Console.WriteLine("Kobieta powyżej trzydziestu trzech lat");
+        }
+        else if (age < 33)
+        {
+            Console.WriteLine("Kobieta poniżej trzydziestu trzech lat");
+        }
+    }
+}
+else
+{
+    if (age == 33)
+    {
+        Console.WriteLine("Ktoś w wieku 33");
+    }
+    else if (age > 33)
+    {
+        Console.WriteLine("Ktoś powyżej trzydziestu trzech lat");
+    }
+    else if (age < 33)
+    {
+        Console.WriteLine("Ktoś poniżej trzydziestu trzech lat");
+    }
+
+}
